3bb140a3b4e8823899b78d7d9c329264a31da16f
Jared E Jackson <jaredjxn15@gmail.com>
2018-11-07 16:33:51 -0500

    Refactored navbar component

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,51 +3,34 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {logout} from '../store';
-import { Menu, Segment, Dropdown } from 'semantic-ui-react';
+import { Menu, Segment} from 'semantic-ui-react';
 
-const Navbar = ({ handleClick, isLoggedIn}) => {
+const Navbar = ({handleClick, isLoggedIn}) => {
   return (
     <div>
     <Segment inverted>
-      {/* The navbar will show these links after you log in */}
       <Menu inverted>
         <Menu.Item as={Link} to="/" name="Home" />
         <Menu.Item as={Link} to="/all-stations" name="All Stations" />
         <Menu.Item as={Link} to="/all-broadcasts" name="All Broadcasts" />
-        {/* The navbar will show these links after you log in */}
-        {
-        isLoggedIn &&
+        {isLoggedIn ?
           <div className="flex">
             <Menu.Item as={Link} to="/myStations" name="My Stations" />
             <Menu.Item as={Link} to="/new-station" name="Create a New Station" />
             <Menu.Item name="Logout" onClick={handleClick} />
-            <Dropdown text="Settings" className="link item">
-              <Dropdown.Menu>
-                <Dropdown.Item as={Link} to="/myAccount">My Account</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-        }
-        {/* The navbar will show these links before you log in */}
-        {
-        !isLoggedIn &&
+            <Menu.Item as={Link} to="/myAccount" name="My Account" />
+          </div> :
           <div className="flex">
             <Menu.Item as={Link} to="/login" name="login" />
             <Menu.Item as={Link} to="/signup" name="Sign up" />
-          </div>
-        }
-          {/* The navbar will show these links before you log in */}
-          {/*<Menu.Menu position="right">
-            <Menu.Item as={Link} to="/broadcasts/1" name="Random Live Cast" />
-      </Menu.Menu>*/}
+          </div>}
         </Menu>
       </Segment>
     </div>
   );
 };
-/**
- * CONTAINER
- */
+
+/*CONTAINER*/
 const mapState = state => {
   return {
     isLoggedIn: !!state.user.id,
@@ -65,9 +48,7 @@ const mapDispatch = dispatch => {
 
 export default connect(mapState, mapDispatch)(Navbar);
 
-/**
- * PROP TYPES
- */
+/*PROP TYPES*/
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
